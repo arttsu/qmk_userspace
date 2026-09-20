27144d075fe48770c5ee7c42d4d6da4009299a45
Artem Tseranu <git@arttsu.com>
2026-09-20 21:37:59 +0200

    Clean up system layer

--- keyboards/planck/keymaps/arttsu/keymap.c
+++ keyboards/planck/keymaps/arttsu/keymap.c
@@ -9,7 +9,6 @@
  * _MEDIA
  * _SYS    - Numpad + firmware/system controls
  * _PLAIN  - Plain QWERTY fallback layer
- * _RETURN - Return from _PLAIN to _BASE
  */
 enum layer_names {
     _BASE,
@@ -18,7 +17,6 @@ enum layer_names {
     _MEDIA,
     _SYS,
     _PLAIN,
-    _RETURN
 };
 
 /*
@@ -78,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F13,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F16,   KC_ASTR,  KC_7,     KC_8,     KC_9,     KC_SLSH,  XXXXXXX,
         KC_F14,   KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F17,   KC_MINS,  KC_4,     KC_5,     KC_6,     KC_0,     KC_COMM,
         KC_F15,   KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_F18,   KC_PLUS,  KC_1,     KC_2,     KC_3,     KC_DOT,   XXXXXXX,
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,            MO(_SYS),           XXXXXXX,  XXXXXXX,  XXXXXXX,  CW_TOGG
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            MO(_SYS),           XXXXXXX,  XXXXXXX,  XXXXXXX,  CW_TOGG
     ),
 
 
@@ -89,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LBRC,  KC_RBRC,  XXXXXXX,
         XXXXXXX,  KC_PLUS,  KC_EQL,   KC_UNDS,  KC_MINS,  XXXXXXX,  XXXXXXX,  KC_LPRN,  KC_RPRN,  KC_LCBR,  KC_RCBR,  XXXXXXX,
         XXXXXXX,  KC_LT,    KC_GT,    KC_PIPE,  KC_BSLS,  XXXXXXX,  XXXXXXX,  KC_GRV,   KC_COMM,  KC_DOT,   KC_SLSH,  XXXXXXX,
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,            XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  MO(_SYS),           XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
     ),
 
 
@@ -106,45 +104,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /*
      * SYS
-     *
-     * Contains firmware/system operations.
-     *
-     * QK_RBT  = reboot keyboard
-     * QK_BOOT = enter bootloader
-     * DF(...) = change persistent default layer
-     * AG_TOGG = swap Alt / GUI
      */
     [_SYS] = LAYOUT_planck_2x2u(
-        QK_RBT,      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_P7,    KC_P8,    KC_P9,    XXXXXXX,  KC_NUM,
-        QK_BOOT,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_P4,    KC_P5,    KC_P6,    KC_P0,    XXXXXXX,
-        XXXXXXX,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_P1,    KC_P2,    KC_P3,    XXXXXXX,  XXXXXXX,
-        DF(_PLAIN),  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,            XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  AG_TOGG
+        QK_BOOT,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+        XXXXXXX,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+        XXXXXXX,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+        TO(_PLAIN),  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
     ),
 
 
     /*
      * PLAIN
-     *
-     * Simple QWERTY fallback layer without home-row mods.
      */
     [_PLAIN] = LAYOUT_planck_2x2u(
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-        KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,
-        XXXXXXX,  KC_LCTL,  KC_LGUI,  KC_LALT,  KC_SPC,             MO(_RETURN),        KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT
-    ),
-
-
-    /*
-     * RETURN
-     *
-     * Used from _PLAIN to restore _BASE as the persistent default layer.
-     */
-    [_RETURN] = LAYOUT_planck_2x2u(
-        XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-        XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-        XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-        DF(_BASE), XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,            XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+        KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
+        KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+        KC_LSFT,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,
+        TO(_BASE),  KC_LCTL,  KC_LGUI,  KC_LALT,  KC_SPC,             KC_SPC,             KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT
     )
 
 };
